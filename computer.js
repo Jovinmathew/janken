fa8c5614764ccbf83546e98daf46bd23229cfcff
Jovinmathew <jovinmathew01@gmail.com>
2024-05-21 21:31:53 +0530

    Added: the basic functions for the game logic

---
+++ computer.js
@@ -0,0 +1,23 @@
+const moves = ['rock', 'paper', 'scissor'];
+let humanScore = 0, computerScore = 0;
+
+function getComputerChoice() {
+    return moves[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+    return prompt("Rock/Paper/Scissor").toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissor' && computerChoice == 'paper' || humanChoice == 'rock' && computerChoice == 'scissor') {
+        console.log('You win', humanChoice, 'beats', computerChoice);
+    }
+    else if (humanChoice == computerChoice) {
+        console.log("It's a draw");
+    }
+    else {
+        console.log('You lose', computerChoice, 'beats', humanChoice);
+    }
+}
+playRound(getHumanChoice(), getComputerChoice());
